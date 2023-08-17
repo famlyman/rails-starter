059fe6e91f8590a7c0f4bbd30f80a32bae923c9a
famlyman <burkecustomcomputers@gmail.com>
2023-08-16 21:59:58 -0400

    init commit

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+enum role: {player: 0, captain: 1, admin: 2}
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+  devise_for :users
+  
+  resources :admin, only: [:index, :create, :update]
+
+  
+end
